b829ba5e53e3feebf247378eef32f465e44e67a1
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2024-04-07 14:19:53 -0300

    created new enpoints

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import lombok.RequiredArgsConstructor;
+import rocketseat.com.passin.dto.attendee.AttendeeIdDTO;
+import rocketseat.com.passin.dto.attendee.AttendeeRequestDTO;
 import rocketseat.com.passin.dto.attendee.AttendeesListResponseDTO;
 import rocketseat.com.passin.dto.event.EventIdDTO;
 import rocketseat.com.passin.dto.event.EventRequestDTO;
@@ -23,7 +25,7 @@ import rocketseat.com.passin.services.EventService;
 public class EventController {
     private final EventService eventService;
     private final AttendeeService attendeeService;
-    
+
     @GetMapping("/{id}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
         EventResponseDTO event = this.eventService.getEventDetail(id);
@@ -37,6 +39,15 @@ public class EventController {
         var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
 
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<AttendeeIdDTO> registerParticipant(@PathVariable String eventId, @RequestBody AttendeeRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+        AttendeeIdDTO attendeeIdDTO = this.eventService.registerAttendeeOnEvent(eventId, body);
+
+        var uri = uriComponentsBuilder.path("/attendees/{attendId}/badge").buildAndExpand(attendeeIdDTO.attendeeId()).toUri();
+
+        return ResponseEntity.created(uri).body(attendeeIdDTO);
     }
 
     @GetMapping("/attendees/{id}")
